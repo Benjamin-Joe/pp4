21a9e6b98fa76482bb8d07cf3fd2932ed6909af1
Benjamin-Joe <benjaminjoe43@gmail.com>
2022-01-15 17:15:42 +0000

    Add comment form to views.py. Add front end code for comment form in post_detail.html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 "Main views.py file"
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from .models import Post
+from .forms import CommentForm
 
 
 def homepage(request):
@@ -13,4 +14,15 @@ def homepage(request):
 def post_detail(request, post):
     "View for individual blog posts"
     post = get_object_or_404(Post, slug=post, status='1')
-    return render(request, 'post_detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    user_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            user_comment = comment_form.save(commit=False)
+            user_comment.post = post
+            user_comment.save()
+            return HttpResponseRedirect('/' + post.slug)
+    else:
+        comment_form = CommentForm()
+    return render(request, 'post_detail.html', {'post': post, 'comments': user_comment, 'comments': comments, 'comment_form': comment_form})
